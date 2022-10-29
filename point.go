d823001cb78c61f67a88a47daaa181b18780358a
Francis Osei Annin <francisannin5@gmail.com>
2022-10-29 19:42:55 +0000

    Point dimension incomplete

--- point.go
+++ point.go
@@ -1,5 +1,11 @@
-// A point is the most fundamental object in geometry with a zero-dimension
+/*
+A point is the most fundamental object in geometry with a zero-dimension.
+In classical eclidean geometry, a point is a primitive notion that models 
+an exact location in space.
+*/
 package siffa  
+
+import "wkt.go"
 
 // Create point type 
 type Point struct{ 
@@ -24,4 +30,9 @@ func width(point Point) float64{
 
 func Centriod(point Point) float64{
 	return 0.0
+}
+
+// returns the dimensions of a point in a well known text format.
+func Dim(point Point){
+
 }
---
+++ wkt.go
@@ -0,0 +1,12 @@
+package siffa
+
+import (
+	"point.go"
+)
+
+
+// Takes a point of type struct and returns the wkt representation
+func Pointstruct2wkt(object point.Point){
+	
+
+}
