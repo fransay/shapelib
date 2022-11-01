6a890e314d37c5f64e1f53b092bd012e2e7cd330
Francis Osei Annin <francisannin5@gmail.com>
2022-11-01 21:55:42 +0000

    Initialised 2D standalone functions

---
+++ functs/area.go
@@ -0,0 +1,20 @@
+package functs
+
+// 2D figures
+
+// Area of a triangle
+// AreaOfTriangleL stands for computation of area
+// using the length properties of the triangle
+func AreaOfTriangleL(side1, side2, side3 float64) {
+
+}
+
+// AreaOfTriangleL represents computation of area
+// using the length properties of the triangle
+func AreaOfTriangleA() {
+
+}
+func AreaOfSquare(length float64) (area float64, err error) {
+	area = length * length
+	return
+}
