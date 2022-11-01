002f65aeaae5b896841e1d3b36401d3470d3f87a
Francis Osei Annin <francisannin5@gmail.com>
2022-11-01 23:42:02 +0000

    Shape types init

---
+++ types/2d.go
@@ -0,0 +1,29 @@
+// spells us two dimensional types
+package types
+
+// type of a Point
+type Point struct {
+	X float64
+	Y float64
+}
+
+// type of triangle
+type Triangle struct {
+	sideA  float64
+	sideB  float64
+	sideC  float64
+	height float64
+}
+
+// type of square
+type Square struct {
+	length float64
+}
+
+// type of rectangle
+type Rectangle struct {
+	length float64
+	width  float64
+}
+
+// type of parallelogram
