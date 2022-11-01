c55282a121f969089ee2a1a08ff0dcfbed4ad9b2
Francis Osei Annin <francisannin5@gmail.com>
2022-11-01 22:35:45 +0000

    Area of triangle function complete

--- functs/area.go
+++ functs/area.go
@@ -1,20 +1,40 @@
+// Package functs provide basic functions for geometric
+// related problems
+
 package functs
+
+import "math"
 
 // 2D figures
 
 // Area of a triangle
-// AreaOfTriangleL stands for computation of area
+
+// AreaOfTriangle represent computation of area
+// using the base and height properties of the triangle
+
+func AreaOfTriangle(base, height float64) (area float64) {
+	area = base * height
+	return area
+
+}
+
+// AreaOfTriangleSide represent computation of area
 // using the length properties of the triangle
-func AreaOfTriangleL(side1, side2, side3 float64) {
+func AreaOfTriangleSide(side1, side2, side3 float64) (area float64) {
+	averageSides := (side1 + side2 + side3) / 3
+	area = math.Sqrt(averageSides * (averageSides-side1) * (averageSides-side2)
+	(averageSides-side3))
+	return area
 
 }
 
 // AreaOfTriangleL represents computation of area
-// using the length properties of the triangle
-func AreaOfTriangleA() {
+// using the angle properties of the triangle
+func AreaOfTriangleAngle() {
 
 }
-func AreaOfSquare(length float64) (area float64, err error) {
+
+func AreaOfSquare(length float64) (area float64) {
 	area = length * length
 	return
 }
