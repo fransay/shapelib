b573f491e3dd150fa2382d626130f3cf1647fb17
fransay <francisannin5@gmail.com>
2022-11-15 19:30:03 +0000

    tests

--- perimeter_test.go
+++ perimeter_test.go
@@ -1,1 +1,15 @@
 package shapelib
+
+import (
+	"math"
+	functs "shapelib/functs/2D"
+	"testing"
+)
+
+func TestPerimeterOfTriangle(t *testing.T) {
+	res := functs.PerimOfRTriangle(10.0, 10.0, 10.0)
+	exp := math.Trunc(30)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
