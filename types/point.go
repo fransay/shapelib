c0857fc686fa87e88ce451a0ef99e092132ddfce
fransay <francisannin5@gmail.com>
2022-11-21 09:32:01 +0000

    Homogeneous coordinates

--- types/point.go
+++ types/point.go
@@ -1,16 +1,39 @@
 package types
 
-// Point 2D point type
-type Point struct {
+// Point2D point type
+// characterise by x and y coordination
+type Point2D struct {
 	X float64
 	Y float64
 }
 
-// PointZ 3D point type
-type PointZ struct {
+// Point3D PointZ 3D point type
+// characterise by x,y and z coordination
+type Point3D struct {
 	X float64
 	Y float64
 	Z float64
+}
+
+// Point2DH Homogeneous 2D point vector
+// when W is 1 PointH type is known as an augmented point vector
+// alternatively when W is 0, PointH type is known as an ideal point
+// or a point at infinity
+type Point2DH struct {
+	X float64
+	Y float64
+	W float64
+}
+
+// Point3DH Homogeneous 2D point vector
+// when W is 1 PointH type is known as an augmented point vector
+// alternatively when W is 0, PointH type is known as an ideal point
+// or a point at infinity
+type Point3DH struct {
+	X float64
+	Y float64
+	z float64
+	W float64
 }
 
 // Point types has no methods
