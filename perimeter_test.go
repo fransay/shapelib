cfa96fcf41bedea999b6c0e6862dff797f9aa81f
Francis Osei Annin <francisannin5@gmail.com>
2022-12-06 18:10:01 +0000

    Perimeter functions passed test

--- perimeter_test.go
+++ perimeter_test.go
@@ -54,3 +54,71 @@ func TestPerimOfRhombus(t *testing.T) {
 		t.Errorf("expected %f, got %f", exp, res)
 	}
 }
+
+func TestPerimOfTrapezoid(t *testing.T) {
+	res := functs.PerimOfRTrapezoid(5.0, 10.0, 5.0, 20.0)
+	exp := math.Trunc(40.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+func TestPerimOfPentagon(t *testing.T) {
+	res := functs.PerimOfRPentagon(5.0)
+	exp := math.Trunc(25.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+func TestPerimOfHexagon(t *testing.T) {
+	res := functs.PerimOfRHexagon(5.0)
+	exp := math.Trunc(30.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+func TestPerimOfHeptagon(t *testing.T) {
+	res := functs.PerimOfRHeptagon(5.0)
+	exp := math.Trunc(35.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+func TestPerimOfOctagon(t *testing.T) {
+	res := functs.PerimOfROctagon(5.0)
+	exp := math.Trunc(40.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+
+func TestPerimOfNonagon(t *testing.T) {
+	res := functs.PerimOfRNonagon(5.0)
+	exp := math.Trunc(45.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+
+func TestPerimOfDecagon(t *testing.T) {
+	res := functs.PerimOfRDecagon(5.0)
+	exp := math.Trunc(50.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+
+func TestPerimOfRegularGeneric(t *testing.T) {
+	res := functs.PerimOfRegularGeneric(5.0, 15.0)
+	exp := math.Trunc(75.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
+
+func TestPerimOfIrregularGeneric(t *testing.T) {
+	res := functs.PerimOfIrregularGeneric(5.0, 6.0, 5.0, 7.0, 8.0)
+	exp := math.Trunc(31.0)
+	if math.Trunc(res) != exp {
+		t.Errorf("expected %f, got %f", exp, res)
+	}
+}
