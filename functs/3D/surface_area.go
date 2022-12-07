387e6e94aa815c37495178f696fded1e2be295d0
Francis Osei Annin <francisannin5@gmail.com>
2022-12-07 16:54:07 +0000

    Surf Area functions updated

--- functs/3D/surface_area.go
+++ functs/3D/surface_area.go
@@ -1,8 +1,33 @@
 // Package functs computes the surface area of 3D geometries
 package functs
 
+import "math"
+
+// SurfAreaOfTetrahedron :surface area of a tetrahedron
+func SurfAreaOfTetrahedron() (surfArea float64) {
+
+	return surfArea
+
+}
+
+// SurfAreaOfRightAngledPyramid :surface area of a right-angled pyramid
+func SurfAreaOfRightAngledPyramid(baseLength float64, baseWidth float64, pyramidHeight float64) (surfArea float64) {
+	surfArea = (baseLength * baseWidth) + baseLength*(math.Sqrt(((baseWidth/2)*(baseWidth/2))+(pyramidHeight*pyramidHeight))) +
+		baseWidth*(math.Sqrt(((baseLength/2)*(baseLength/2))+(pyramidHeight*pyramidHeight)))
+	return surfArea
+
+}
+
 // SurfAreaOfCube :surface area of a cube
 func SurfAreaOfCube(sideLength float64) (surfArea float64) {
 	surfArea = 6 * sideLength
 	return surfArea
 }
+
+// SurfAreaOfCuboid :surface area of cuboid
+func SurfAreaOfCuboid(sideLength, sideWidth, sideHeigth float64) (surfArea float64) {
+	surfArea = 2*(sideLength) + 2*(sideWidth) + 2*(sideHeigth)
+	return surfArea
+}
+
+// surface
