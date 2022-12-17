4a124c216ab94de6d39ee2265eeb71e79c90ea15
Francis Osei Annin <francisannin5@gmail.com>
2022-12-17 20:42:09 +0000

    Volume of Tetrahedron test passed :rocket:

--- volume_test.go
+++ volume_test.go
@@ -1,1 +1,16 @@
 package shapelib
+
+import (
+	"math"
+	functs "shapelib/functs/3D"
+	"testing"
+)
+
+func TestVolOfTetrahedron(t *testing.T) {
+	var result = functs.VolOfTetrahedron(10)
+	var expected = math.Trunc(117.80)
+	if math.Trunc(result) != expected {
+		t.Errorf("expected %v got %v", expected, result)
+	}
+
+}
