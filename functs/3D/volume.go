5a4487a3baa9d92478f08c294ad145351860f59f
Francis Osei Annin <francisannin5@gmail.com>
2023-01-01 10:57:59 +0000

    Refactored volume functions :rocket:

--- functs/3D/volume.go
+++ functs/3D/volume.go
@@ -4,21 +4,13 @@ import (
 	"math"
 )
 
-<<<<<<< HEAD
-// VolOfTetrahedron returns the volume of a tetrahedron
-=======
 // VolOfTetrahedron :volume of a tetrahedron
->>>>>>> 88d1fd7eef22a2b93a3aaf013a8d37f04a884d09
 func VolOfTetrahedron(side float64) (vol float64) {
 	vol = (side * side * side) / (6 * math.Sqrt(2))
 	return vol
 }
 
-<<<<<<< HEAD
-// VolOfRightAngledTriPyramid returns the volume of a right-angled triangular pyramid
-=======
 // VolOfRightAngledTriPyramid volume of a right-angled pyramid
->>>>>>> 88d1fd7eef22a2b93a3aaf013a8d37f04a884d09
 func VolOfRightAngledTriPyramid(baseBaseTriangle, heightBaseTriangle, heightPyramid float64) (vol float64) {
 	vol = (baseBaseTriangle * heightBaseTriangle * heightPyramid) / 6
 	return vol
