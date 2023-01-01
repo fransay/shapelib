eb88ee2ca056e284bb6baaba4cd17d31dd8aede5
Francis Osei Annin <francisannin5@gmail.com>
2023-01-01 10:53:09 +0000

    Added comments to functions :rocket:

--- functs/3D/volume.go
+++ functs/3D/volume.go
@@ -2,11 +2,13 @@ package functs
 
 import "math"
 
+// VolOfTetrahedron returns the volume of a tetrahedron
 func VolOfTetrahedron(side float64) (vol float64) {
 	vol = (side * side * side) / (6 * math.Sqrt(2))
 	return vol
 }
 
+// VolOfRightAngledTriPyramid returns the volume of a right-angled triangular pyramid
 func VolOfRightAngledTriPyramid(baseBaseTriangle, heightBaseTriangle, heightPyramid float64) (vol float64) {
 	vol = (baseBaseTriangle * heightBaseTriangle * heightPyramid) / 6
 	return vol
