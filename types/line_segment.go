b76b367a9100dac7711a2d7bcc147882b2736d64
fransay <francisannin5@gmail.com>
2023-01-01 23:15:02 +0000

    Line segment complete :rocket:

--- types/line_segment.go
+++ types/line_segment.go
@@ -2,33 +2,34 @@ package types
 
 import "math"
 
+// LineSegment type
 type LineSegment struct {
 	PointA Point2D
 	PointB Point2D
 }
 
-// Distance :euclidean distance of a line segment
+// Distance euclidean distance of a line segment
 func (l *LineSegment) Distance() (dist float64) {
-	deltaX := l.PointB.X - l.PointB.X
-	deltaY := l.PointB.Y - l.PointB.Y
-	dist = math.Sqrt(math.Pow(deltaX, 2) + math.Pow(deltaY, 2))
+	deltaX := math.Abs(l.PointB.X - l.PointA.X)
+	deltaY := math.Abs(l.PointB.Y - l.PointA.Y)
+	dist = math.Sqrt((deltaX * deltaX) + (deltaY * deltaY))
 	return dist
 
 }
 
-// MidPoint :midpoint of a line segment
+// MidPoint midpoint of a line segment
 func (l *LineSegment) MidPoint() Point2D {
-	deltaX := (l.PointB.X + l.PointB.X) / 2
-	deltaY := (l.PointB.Y + l.PointB.Y) / 2
+	deltaX := (l.PointB.X + l.PointA.X) / 2
+	deltaY := (l.PointB.Y + l.PointA.Y) / 2
 	midPoint := Point2D{deltaX, deltaY}
 	return midPoint
 
 }
 
-// Bearing :bearing of the line segment in a vector space
+// Bearing of the line segment in a vector space
 func (l *LineSegment) Bearing() (bearing float64) {
-	deltaX := l.PointB.X - l.PointB.X
-	deltaY := l.PointB.Y - l.PointB.Y
+	deltaX := l.PointB.X - l.PointA.X
+	deltaY := l.PointB.Y - l.PointA.Y
 	bearing = math.Atan(deltaX / deltaY)
 	return bearing
 }
