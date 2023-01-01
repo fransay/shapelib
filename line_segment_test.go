4e6514e292c512c39a3a51ac81b896643b177e00
fransay <francisannin5@gmail.com>
2023-01-01 20:12:33 +0000

    TestLineSegmentDistance function passed test :white_check_mark:

--- line_segment_test.go
+++ line_segment_test.go
@@ -1,1 +1,42 @@
 package shapelib
+
+import (
+	"math"
+	"shapelib/types"
+	"testing"
+)
+
+// distance test
+func TestLineSegmentDistance(t *testing.T) {
+	// negative outcomes square under square roots
+	var lineSegmentOne types.LineSegment = types.LineSegment{PointA: struct {
+		X float64
+		Y float64
+	}{X: 5, Y: 10}, PointB: struct {
+		X float64
+		Y float64
+	}{X: 10, Y: 15}}
+
+	var lineSegmentTwo types.LineSegment = types.LineSegment{PointA: struct {
+		X float64
+		Y float64
+	}{X: 234.543, Y: 24043.354}, PointB: struct {
+		X float64
+		Y float64
+	}{X: 544.654, Y: 354.544}}
+
+	// line segment result check
+	var distanceResultLineSegmentOne = lineSegmentOne.Distance()
+	var distanceExpectedLineSegmentOne = 7.017
+	if math.Trunc(distanceResultLineSegmentOne) != math.Trunc(distanceExpectedLineSegmentOne) {
+		t.Errorf("Expected %f, got %f", distanceExpectedLineSegmentOne, distanceResultLineSegmentOne)
+	}
+
+	// line segment two results
+	var distanceResultLineSegmentTwo = lineSegmentTwo.Distance()
+	var distanceExpectedLineSegmentTwo = 23690.84
+	if math.Trunc(distanceResultLineSegmentTwo) != math.Trunc(distanceExpectedLineSegmentTwo) {
+		t.Errorf("Expected %f, got %f", distanceExpectedLineSegmentTwo, distanceResultLineSegmentTwo)
+	}
+
+}
