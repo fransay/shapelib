428656cea4acee33690ba5d29b61550fde56a4ab
Francis Osei Annin <francisannin5@gmail.com>
2023-01-02 22:11:25 +0000

    Test functions initialised

--- line_string_test.go
+++ line_string_test.go
@@ -1,1 +1,18 @@
 package shapelib
+
+import "testing"
+
+// test line string index
+func TestLineStringIndex(t *testing.T) {
+
+}
+
+// test line string number of line segments
+func TestLineStringNumberOfLineSegment(t *testing.T) {
+
+}
+
+// test line string length
+func TestLineStringLength(t *testing.T) {
+
+}
