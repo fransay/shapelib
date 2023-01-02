6c439e7ffb5a9218e9d94cf317604c1a40956549
Francis Osei Annin <francisannin5@gmail.com>
2023-01-02 22:16:35 +0000

    Polygon methods init

--- types/polygon.go
+++ types/polygon.go
@@ -5,6 +5,17 @@ type Polygon []Point2D
 
 // polygon methods
 
+// Area returns the area of the polygon
 func (p *Polygon) Area() (area float64) {
+	return area
+}
+
+// Centroid returns the centroid of the polygon
+func (p *Polygon) Centroid() (cent float64) {
+	return cent
+}
+
+// Perimeter returns total distance around the polygon
+func (p *Polygon) Perimeter() (area float64) {
 	return area
 }
