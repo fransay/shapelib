1b303a0125bc09ae9cb6033e3e8402721c77e5df
fransay <francisannin5@gmail.com>
2023-01-02 00:01:13 +0000

    Refactored comments

--- volume_test.go
+++ volume_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 )
 
+// test volume of tetrahedron
 func TestVolOfTetrahedron(t *testing.T) {
 	var result = functs.VolOfTetrahedron(10)
 	var expected = math.Trunc(117.80)
@@ -14,8 +15,40 @@ func TestVolOfTetrahedron(t *testing.T) {
 	}
 
 }
+
+// test volume of right angle triangular pyramid
 func TestVolOfRightAngledTriPyramid(t *testing.T) {
 	var result = functs.VolOfRightAngledTriPyramid(10, 10, 10)
+	var expected = math.Trunc(166.666)
+	if math.Trunc(result) != expected {
+		t.Errorf("expected %v got %v", expected, result)
+	}
+
+}
+
+// test volume of cube
+func TestVolOfCube(t *testing.T) {
+	var result = functs.VolOfCube(10)
+	var expected = math.Trunc(1000.000)
+	if math.Trunc(result) != expected {
+		t.Errorf("expected %v got %v", expected, result)
+	}
+
+}
+
+// test volume of cuboid
+func TestVolOfCuboid(t *testing.T) {
+	var result = functs.VolOfCuboid(10, 5.0, 7.0)
+	var expected = math.Trunc(350.00)
+	if math.Trunc(result) != expected {
+		t.Errorf("expected %v got %v", expected, result)
+	}
+
+}
+
+// test volume of pentahedron
+func TestVolOfPentahedron(t *testing.T) {
+	var result = functs.VolOfPentahedron(10.00, 5.00)
 	var expected = math.Trunc(166.666)
 	if math.Trunc(result) != expected {
 		t.Errorf("expected %v got %v", expected, result)
