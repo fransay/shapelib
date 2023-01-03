75b09f5c140748330116f59b91248eaf59db6441
Francis Osei Annin <francisannin5@gmail.com>
2023-01-03 21:48:19 +0000

    Test polygon line segment function passed :rocket:

--- polygon_test.go
+++ polygon_test.go
@@ -16,3 +16,13 @@ func TestPolygonNumberOfNodes(t *testing.T) {
 }
 
 // number of line segment test
+func TestPolygonLineSegment(t *testing.T) {
+	var polygonOne = types.Polygon{types.Point2D{X: 7, Y: 9}, types.Point2D{X: 2, Y: 6}, types.Point2D{X: 4, Y: 8}}
+	var numberOfLineSegmentResult = polygonOne.NumberOfLineSegments()
+	var numberOfLineSegmentExpected = 3
+	if numberOfLineSegmentExpected != numberOfLineSegmentResult {
+		t.Errorf("Expected %d, got %d", numberOfLineSegmentExpected, numberOfLineSegmentResult)
+	}
+}
+
+//
