4e0394bf545d3e71261d9da1ab9557877c5712f9
Francis Osei Annin <francisannin5@gmail.com>
2023-01-03 21:23:47 +0000

    Number of nodes test function passed!

---
+++ polygon_test.go
@@ -0,0 +1,17 @@
+package shapelib
+
+import (
+	"shapelib/types"
+	"testing"
+)
+
+// number of nodes test
+func TestPolygonNumberOfNodes(t *testing.T) {
+	var polygonOne = types.Polygon{types.Point2D{X: 7, Y: 9}, types.Point2D{X: 2, Y: 6}, types.Point2D{X: 4, Y: 8}}
+	// testing number of nodes
+	var numberOfNodesResult = polygonOne.NumberOfNodes()
+	var numberOfNodesExpected = 3
+	if numberOfNodesResult != numberOfNodesExpected {
+		t.Errorf("Expected %d, got %d", numberOfNodesExpected, numberOfNodesResult)
+	}
+}
