7318fb2c5d68c0bb65d57fbe8b92281ec824fac1
Francis Osei Annin <francisannin5@gmail.com>
2023-01-05 23:48:06 +0000

    Linear distance function refactored

--- functs/2D/distance.go
+++ functs/2D/distance.go
@@ -5,8 +5,8 @@ import (
 	"shapelib/types"
 )
 
-// Distance returns distance
-func Distance(pointOne, pointTwo types.Point2D) (dist float64) {
+// EDistance returns euclidean distance between two points/coordinates
+func EDistance(pointOne, pointTwo types.Point2D) (dist float64) {
 	deltaX := pointTwo.X - pointOne.X
 	deltaY := pointOne.Y - pointOne.Y
 	dist = math.Sqrt(math.Pow(deltaX, 2) + math.Pow(deltaY, 2))
