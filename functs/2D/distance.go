f644397b0b0ece442a80e66c04dcf72cda424880
Francis Osei Annin <francisannin5@gmail.com>
2023-01-07 20:40:43 +0000

    Minkowski distance completed :rocket:

--- functs/2D/distance.go
+++ functs/2D/distance.go
@@ -14,7 +14,8 @@ func EDistance(pointOne, pointTwo types.Point2D) (dist float64) {
 }
 
 // MinkowsiDistance returns minkowsi distance of points in an N
-// dimensional space
+// dimensional space, basically an approx of euclidean and
+// mahanttan distance.
 func MinkowsiDistance(PointA, PointB types.Point2D, p float64) (distMinkowsi float64) {
 	var deltaA = math.Pow(PointA.X-PointA.Y, p)
 	var deltaB = math.Pow(PointB.X-PointB.Y, p)
