d65020b9f95134567859dbe41a506469d49a9db9
Francis Osei Annin <francisannin5@gmail.com>
2023-03-13 17:54:20 +0000

    Translation functions added

--- transformations/translate.go
+++ transformations/translate.go
@@ -2,7 +2,28 @@
 
 package transformations
 
-// translate a point type
+import "shapelib/types"
+
+// Translate2D translate a 2D point type
+// translate 2D point
+func Translate2D(point, shiftVector types.Point2D) (translate types.Point2D) {
+	translateX := shiftVector.X + point.X
+	translateY := shiftVector.Y + point.Y
+	translate = types.Point2D{X: translateX, Y: translateY}
+	return translate
+
+}
+
+// Translate3D translate a 3D point type
+func Translate3D(point, shiftVector types.Point3D) (translate types.Point3D) {
+	translateX := shiftVector.X + point.X
+	translateY := shiftVector.Y + point.Y
+	translateZ := shiftVector.Z + point.Z
+	translate = types.Point3D{X: translateX, Y: translateY, Z: translateZ}
+	return translate
+
+}
+
 // translate a line segment type
 // translate a linestring type
 // translate a polygon type
