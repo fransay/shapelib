668541fde69a34e417f57035f9b87f88548bdb92
Francis Osei Annin <francisannin5@gmail.com>
2023-03-13 08:16:15 +0000

    Translation functions declared

--- transformations/translate.go
+++ transformations/translate.go
@@ -1,1 +1,8 @@
+// generic functions to translate geometric in a vector space
+
 package transformations
+
+// translate a point type
+// translate a line segment type
+// translate a linestring type
+// translate a polygon type
--- types/point.go
+++ types/point.go
@@ -36,4 +36,54 @@ type Point3DH struct {
 	W float64
 }
 
-// Point types has no methods
+// Point methods
+
+// Size of a point2D is negligible
+func (p *Point2D) Size() (size float64) {
+	size = 0.0
+	return size
+}
+
+// Coordinates of point2D type returned in an array
+func (p *Point2D) Coordinates() (coordinates []float64) {
+	coordinates = []float64{p.X, p.Y}
+	return coordinates
+}
+
+// Size of point2DH type is negligible
+func (p *Point2DH) Size() (size float64) {
+	size = 0.0
+	return size
+}
+
+// Coordinates of point2DH type returned in an array(X,Y,W}
+func (p *Point2DH) Coordinates() (coordinates []float64) {
+	coordinates = []float64{p.X, p.Y, p.W}
+	return coordinates
+}
+
+func (p *Point3D) Size() (size float64) {
+	size = 0.0
+	return size
+}
+
+// Coordinates of point3D type returned in an array{X,Y,Z} format
+func (p *Point3D) Coordinates() (coordinates []float64) {
+	coordinates = []float64{p.X, p.Y, p.Z}
+	return coordinates
+}
+
+// Size of a 3D homogenous point
+// the size of a 3D homogenous point is almost negligible
+// 0.0 is returned as negligible
+func (p *Point3DH) Size() (size float64) {
+	size = 0.0
+	return size
+}
+
+// Translate a point
+// Translate function takes an input scalar
+// and return a new point
+func (p *Point2D) Translate(shiftScalar float64) {
+	// translation implementation
+}
