a4decf00a1328c131bfe7cfdef3d72635f4ef665
Francis Osei Annin <francisannin5@gmail.com>
2023-11-25 04:51:03 +0000

    test: all tests passed

--- cartesian_test.go
+++ cartesian_test.go
@@ -1,1 +1,24 @@
 package shapelib
+
+import (
+	cs "shapelib/coord-sys"
+	"testing"
+)
+
+func TestToPolar(t *testing.T) {
+	cartObj := cs.Cart{X: 20, Y: 5}
+	obs := cartObj.ToPolar()
+	exp := cs.Polar{Distance: 20.615528128088304, Angle: 1.3258176636680323}
+	if obs != exp {
+		t.Errorf("expected %f, got %f", exp, obs)
+	}
+}
+
+func TestPoint2PointDist(t *testing.T) {
+	cartObj := cs.Cart{X: 20, Y: 5}
+	obs := cartObj.Point2PointDistance(cs.Cart{X: 10, Y: 5})
+	exp := 10.0
+	if obs != exp {
+		t.Errorf("expected %f, got %f", exp, obs)
+	}
+}
