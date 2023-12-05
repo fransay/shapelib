cabac5a95a939a76d8df3bbe069ebd0d386fafcf
Francis Osei Annin <francisannin5@gmail.com>
2023-12-05 17:57:32 +0000

    refactor: refactor Point2PointDistanceMethod

--- coord-sys/spherical.go
+++ coord-sys/spherical.go
@@ -1,10 +1,12 @@
 package coord_sys
 
+import "math"
+
 // Spherical type
 type Spherical struct {
 	RadialDistance float64
-	PolarAngle     float64 // in degrees
-	AzimuthAngle   float64 // in degrees
+	PolarAngle     float64 // in radians
+	AzimuthAngle   float64 // in radians
 }
 
 // ToCartesian converts spherical coordinate to cartesian coordinates
@@ -15,5 +17,10 @@ func (s *Spherical) ToCartesian() (cart Cart) {
 // Point2PointDistance returns distance two spherical points
 // using spherical laws of cosine
 func (s *Spherical) Point2PointDistance(point Spherical) (dist float64) {
+	addRadialDist := s.RadialDistance + point.RadialDistance
+	prodRadialDist := s.RadialDistance * point.RadialDistance
+	angArithmetic := math.Sin(s.AzimuthAngle)*math.Sin(point.AzimuthAngle)*math.Cos(s.PolarAngle-point.PolarAngle) +
+		math.Cos(s.AzimuthAngle)*math.Cos(point.AzimuthAngle)
+	dist = math.Sqrt(addRadialDist - 2*prodRadialDist*angArithmetic)
 	return dist
 }
