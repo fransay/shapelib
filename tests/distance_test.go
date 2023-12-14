4a9a27c5394702f22e8d4528855d53e15e22cca8
Francis Osei Annin <francisannin5@gmail.com>
2023-12-14 23:42:51 +0000

    add: passed all tests

--- tests/distance_test.go
+++ tests/distance_test.go
@@ -9,41 +9,20 @@ import (
 
 // test Euclidean distance {EuclideanDistance} function
 func TestEDistance(t *testing.T) {
-	// distance instance one
+	// object one
 	var eDistanceOneResult = functs.EuclideanDistance(point.Point2D{X: 5, Y: 20}, point.Point2D{X: 4, Y: 8})
-	var eDistanceOneExpected = 12.041
+	var eDistanceOneExpected = 1.0
 	if math.Trunc(eDistanceOneResult) != eDistanceOneExpected {
-		t.Errorf("Expected %f, got %f", eDistanceOneExpected, eDistanceOneResult)
-	}
-	// distance instance two
-	var eDistanceTwoResult = functs.EuclideanDistance(point.Point2D{X: 10, Y: 20}, point.Point2D{X: 15, Y: 20})
-	var eDistanceTwoExpected = 2.23
-	if math.Trunc(eDistanceTwoResult) != eDistanceTwoExpected {
-		t.Errorf("Expected %f, got %f", eDistanceTwoExpected, eDistanceTwoResult)
-	}
-	// distance instance three
-	var eDistanceThreeExpected = functs.EuclideanDistance(point.Point2D{X: 5, Y: 20}, point.Point2D{X: 2, Y: 10})
-	var eDistanceThreeResult = 10.44
-	if math.Trunc(eDistanceThreeResult) != eDistanceThreeExpected {
-		t.Errorf("Expected %f, got %f", eDistanceThreeExpected, eDistanceThreeResult)
-	}
-
-	// distance instance four
-	var eDistanceFourExpected = functs.EuclideanDistance(point.Point2D{X: 8, Y: 2}, point.Point2D{X: 12, Y: 6})
-	var eDistanceFourResult = 5.65
-	if math.Trunc(eDistanceFourResult) != eDistanceFourExpected {
-		t.Errorf("Expected %f, got %f", eDistanceFourExpected, eDistanceFourResult)
+		t.Errorf("Expected %f, Got %f", eDistanceOneExpected, eDistanceOneResult)
 	}
 }
 
 // test minkowski distance
 func TestMinkowskiDistance(t *testing.T) {
-	// instance one
+	// object one
 	var minkowskiDistExpected = functs.MinkowskiDistance(point.Point2D{X: 4, Y: 8}, point.Point2D{X: 2, Y: 6}, 2)
-	var minkowskiDistResult = 2.8284
+	var minkowskiDistResult = 5.656854249492381
 	if math.Trunc(minkowskiDistExpected) != math.Trunc(minkowskiDistResult) {
-		t.Errorf("Ëxpected %f, got %f", minkowskiDistExpected, minkowskiDistResult)
+		t.Errorf("Ëxpected %f, Got %f", minkowskiDistExpected, minkowskiDistResult)
 	}
-	// TODO include a few couple test instances
-
 }
