2d6702bab125e238e35e0eb34ec79d92ef0b9d5d
Francis Osei Annin <francisannin5@gmail.com>
2023-12-14 15:25:10 +0000

    add: add deg2rad and rad2deg

--- utils/util.go
+++ utils/util.go
@@ -14,7 +14,6 @@ func EqualSlice(sliceOne, sliceTwo []float64) (equal bool) {
 			equal = false
 			return
 		}
-
 	}
 	equal = true
 	return equal
@@ -24,4 +23,16 @@ func EqualSlice(sliceOne, sliceTwo []float64) (equal bool) {
 func AbsDiff(quantOne, quantTwo float64) (abs float64) {
 	abs = math.Abs(quantTwo - quantOne)
 	return abs
+}
+
+// Rad2Deg convert radians to degree
+func Rad2Deg(radians float64) (deg float64) {
+	deg = radians * (180 / math.Pi)
+	return deg
+}
+
+// Deg2Rad convert degrees to radians
+func Deg2Rad(deg float64) (rad float64) {
+	rad = deg * (math.Pi / 180)
+	return rad
 }
