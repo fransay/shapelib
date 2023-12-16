01a6837246fa682fe2bbbfe0cf15c65fd2a16c3f
Francis Osei Annin <francisannin5@gmail.com>
2023-12-16 01:26:37 +0000

    add: add functions

--- functs/volume.go
+++ functs/volume.go
@@ -4,9 +4,7 @@ import (
 	"math"
 )
 
-// TODO read more about volume of 3D figures
-
-// VolOfTetrahedron volume of a tetrahedron
+// VolOfTetrahedron return volume of a tetrahedron
 func VolOfTetrahedron(side float64) (vol float64) {
 	vol = (side * side * side) / (6 * math.Sqrt(2))
 	return vol
@@ -36,4 +34,88 @@ func VolOfPentahedron(lengthOfBase, height float64) (vol float64) {
 	return vol
 }
 
-// TODO: Explore geometric figures that have not yet be covered
+// VolOfSphere returns the volume of a sphere
+func VolOfSphere(radius float64) (vol float64) {
+	vol = (4 * math.Pi * math.Pow(radius, 3)) / 3
+	return vol
+}
+
+// VolOfCone return volume of a cone
+func VolOfCone() {}
+
+// VolOfCylinder return volume of cylinder
+func VolOfCylinder() {}
+
+// VolOfRectPrism return volume of a rectangular prism
+func VolOfRectPrism() {}
+
+// VolOfTriPrism return volume of triangular prism
+func VolOfTriPrism() {}
+
+// VolOfPentPrism return volume of pentagonal prism
+func VolOfPentPrism() {}
+
+// VolOfHexaPrism return volume of hexagonal prism
+func VolOfHexaPrism() {}
+
+// VolOfOctPrism return volume of octagonal prism
+func VolOfOctPrism() {}
+
+// VolOfSquarePymd return volume of square pyramid
+func VolOfSquarePymd() {}
+
+// VolOfTriPymd return volume of a triangular pyramid
+func VolOfTriPymd(areaOfBase, height float64) (vol float64) {
+	vol = (areaOfBase * height) / 3
+	return vol
+}
+
+// VolOfPentPymd return volume of pentagonal pyramid
+func VolOfPentPymd(baseEdge, height float64) (vol float64) {
+	vol = (5 * math.Tan(54.0) * height * math.Pow(baseEdge, 2)) / 12
+	return vol
+}
+
+// VolOfHexaPymd return volume of hexagonal pyramid
+func VolOfHexaPymd(baseEdge, height float64) (vol float64) {
+	vol = (math.Sqrt(3) * math.Pow(baseEdge, 2) * height) / 2
+	return vol
+}
+
+// VolOfConeFrustum return volume of cone frustrum
+func VolOfConeFrustum(radius, height float64) (vol float64) {
+	vol = math.Pi * math.Pow(radius, 2) * height / 3
+	return vol
+}
+
+// VolOfCyFrustum return volume of a cylinder frustum or right cylinder
+func VolOfCyFrustum(radius, height float64) (vol float64) {
+	vol = math.Pi * math.Pow(radius, 2) * height
+	return vol
+}
+
+// VolOfEllipsoid return volume of ellipsoid
+func VolOfEllipsoid(semiAxisA, semiAxisB, semiAxisC float64) (vol float64) {
+	vol = (4 * math.Pi * semiAxisA * semiAxisB * semiAxisC) / 3
+	return vol
+}
+
+// VolOfTorus return volume of a torus
+func VolOfTorus(majorRadius, minorRadius float64) (vol float64) {
+	vol = math.Pi * math.Pow(minorRadius, 2) * 2 * math.Pi * majorRadius
+	return vol
+}
+
+// VolOfDecahedron return volume of a decahedron
+func VolOfDecahedron(sides float64) (vol float64) {
+	factor := (15 + (7 * math.Sqrt(5.0))) / 4
+	vol = factor * math.Pow(sides, 3)
+	return vol
+}
+
+// VolOfIcosahedron return volume of an icosahedron
+func VolOfIcosahedron(side float64) (vol float64) {
+	factor := 5 * (3.0 + math.Sqrt(5.0)) / 12
+	vol = factor * math.Pow(side, 3)
+	return vol
+}
