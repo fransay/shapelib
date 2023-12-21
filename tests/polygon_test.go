7cb7d50327fd2676a096034d02fab7398c2a5163
Francis Osei Annin <francisannin5@gmail.com>
2023-12-21 05:27:16 +0000

    test: scale fail

--- tests/polygon_test.go
+++ tests/polygon_test.go
@@ -67,3 +67,75 @@ func TestPolygonCentroid(t *testing.T) {
 		t.Errorf("Expected %f, Got %f", centroidThreeExpected, centroidThreeResults)
 	}
 }
+
+// test number of line segments in polygon
+func TestNumberOfLineSegments(t *testing.T) {
+	var polygonOne = s.Polygon{point.Point2D{X: 60, Y: 50}, point.Point2D{X: 22, Y: 65}, point.Point2D{X: 40, Y: 8}}
+	var numberOfLineSegmentsObserved = polygonOne.NumberOfLineSegments()
+	var numberOfLineSegmentsExpected = 2
+	if numberOfLineSegmentsObserved != numberOfLineSegmentsExpected {
+		t.Errorf("Expected %v, Got %v", numberOfLineSegmentsExpected, numberOfLineSegmentsObserved)
+
+	}
+}
+
+// test number of nodes in a polygon
+func TestNumberOfNodes(t *testing.T) {
+	var polygonOne = s.Polygon{point.Point2D{X: 60, Y: 50}, point.Point2D{X: 22, Y: 65}, point.Point2D{X: 40, Y: 8}}
+	var numberOfNodesObserved = polygonOne.NumberOfNodes()
+	var numberOfNodesExpected = 3
+	if numberOfNodesObserved != numberOfNodesExpected {
+		t.Errorf("Expected %v, Got %v", numberOfNodesExpected, numberOfNodesObserved)
+
+	}
+}
+
+// test rotate transformation
+func TestRotate(t *testing.T) {
+	var polygonOne = s.Polygon{point.Point2D{X: 60, Y: 50}, point.Point2D{X: 22, Y: 65}, point.Point2D{X: 40, Y: 8}}
+	var polygonTwo = s.Polygon{point.Point2D{X: 130.71132228476674, Y: 110.83117391955632}, point.Point2D{X: 25.446469962218117, Y: 31.677564464354067}, point.Point2D{X: 446.7709136776561, Y: 302.49969654271126}}
+	var RotatePolygonObserved = polygonOne.Rotate(40)
+	var RotatePolygonExpected = polygonTwo
+	if RotatePolygonExpected.IsEqual(RotatePolygonObserved) {
+		t.Errorf("Expected %v, Got %v", RotatePolygonExpected, RotatePolygonObserved)
+
+	}
+
+}
+
+// test scale transformation
+func TestScale(t *testing.T) {
+	var polygonOne = s.Polygon{point.Point2D{X: 60, Y: 50}, point.Point2D{X: 22, Y: 65}, point.Point2D{X: 40, Y: 8}}
+	var scaledPolygonObserved = polygonOne.Scale([]float64{2.0, 2.0})
+	var scaledPolygonExpected = s.Polygon{point.Point2D{X: 120, Y: 100}, point.Point2D{X: 44, Y: 130}, point.Point2D{X: 80, Y: 16}}
+	if scaledPolygonExpected.IsEqual(scaledPolygonObserved) {
+		t.Errorf("Expected %v, Got %v", scaledPolygonExpected, scaledPolygonObserved)
+
+	}
+}
+
+// test isequal method
+func TestIsEqual(t *testing.T) {
+	var polygonOne = s.Polygon{point.Point2D{X: 60, Y: 50}, point.Point2D{X: 22, Y: 65}, point.Point2D{X: 40, Y: 8}}
+	var polygonTwo = s.Polygon{point.Point2D{X: 10, Y: 20}, point.Point2D{X: 5, Y: 15}, point.Point2D{X: 30, Y: 15}}
+	var IsEqualPolygonObserved = polygonOne.IsEqual(polygonTwo)
+	var IsEqualPolygonExpected = false
+	if IsEqualPolygonObserved != IsEqualPolygonExpected {
+		t.Errorf("Expected %v, Got %v", IsEqualPolygonExpected, IsEqualPolygonObserved)
+
+	}
+
+}
+
+// test contains method
+func TestContains(t *testing.T) {
+	var polygonOne = s.Polygon{point.Point2D{X: 60, Y: 50}, point.Point2D{X: 22, Y: 65}, point.Point2D{X: 40, Y: 8}}
+	var polygonTwo = s.Polygon{point.Point2D{X: 10, Y: 20}, point.Point2D{X: 5, Y: 15}, point.Point2D{X: 30, Y: 15}}
+	var ContainsPolygonObserved = polygonOne.Contains(polygonTwo, point.Point2D{X: 10, Y: 20})
+	var ContainsPolygonExpected = true
+	if ContainsPolygonObserved != ContainsPolygonExpected {
+		t.Errorf("Expected %v, Got %v", ContainsPolygonExpected, ContainsPolygonObserved)
+
+	}
+
+}
