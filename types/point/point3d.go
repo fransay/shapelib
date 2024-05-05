fa07f0765061677b526f23e5ed7829707bfcc291
Francis Osei Annin <francisannin5@gmail.com>
2024-05-05 19:03:34 +0000

    Add: New updates

--- types/point/point3d.go
+++ types/point/point3d.go
@@ -16,7 +16,7 @@ func (p *Point3D) Size() (size float64) {
 func (p *Point3D) Translate3D(shiftVector Point3D) (translate Point3D) {
 	translateX := p.X + shiftVector.X
 	translateY := p.Y + shiftVector.Y
-	translateZ := p.Z + shiftVector.Z
+	translateZ := p.Z + shiftVector.Y
 	translate = Point3D{translateX, translateY, translateZ}
 	return translate
 }
@@ -25,13 +25,4 @@ func (p *Point3D) Translate3D(shiftVector Point3D) (translate Point3D) {
 func (p *Point3D) Coordinates() (coordinates []float64) {
 	coordinates = []float64{p.X, p.Y, p.Z}
 	return coordinates
-}
-
-// Scale returns a transformed point3D under a scaling transformation
-func (p *Point3D) Scale(scalarVector []float64) (scale Point3D) {
-	scale.X = p.X * scalarVector[0]
-	scale.Y = p.Y * scalarVector[1]
-	scale.Z = p.Z * scalarVector[2]
-	return scale
-
 }
