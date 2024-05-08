33bf5433833b881f08f50723794f779792b13cbe
Francis Osei Annin <francisannin5@gmail.com>
2024-05-08 20:34:15 +0000

    Add: Add update

--- tests/pentagon_test.go
+++ tests/pentagon_test.go
@@ -1,13 +1,10 @@
 package types
 
 import (
-	"shapelib/shape"
 	"testing"
 )
 
 func TestPentagon(t *testing.T) {
-	var pentagon = shape.Pentagon{Side: 15}
-
 	// area
 	// perimeter
 	// diagonal
--- tests/rectangle_test.go
+++ tests/rectangle_test.go
@@ -1,8 +1,38 @@
 package types
 
-import "testing"
+import (
+	"shapelib/shape"
+	"testing"
+)
 
 func TestRectangle(t *testing.T) {
-	// rectangle
+	var rectangle = shape.Rectangle{Length: 15, Width: 20}
+	// area by length width
+	resultOfAreaByLengthWidth := rectangle.AreaByLengthWidth()
+	expectedAreaByLengthWidth := 3000.0
+	if resultOfAreaByLengthWidth != expectedAreaByLengthWidth {
+		t.Errorf("Got %f, Expected %f", resultOfAreaByLengthWidth, expectedAreaByLengthWidth)
+	}
+
+	// area by diagonal
+	resultOfAreaByDiagonal := rectangle.AreaByDiagonal(25)
+	expectedOfAreaByDiagonal := 3000.0
+	if resultOfAreaByDiagonal != expectedOfAreaByDiagonal {
+		t.Errorf("Got %f, Expected %f", resultOfAreaByDiagonal, expectedOfAreaByDiagonal)
+	}
+
+	// perimeter
+	resultOfPerimeter := rectangle.Perimeter()
+	expectedOfPerimeter := 70.0
+	if resultOfAreaByDiagonal != expectedOfAreaByDiagonal {
+		t.Errorf("Got %f, Expected %f", resultOfPerimeter, expectedOfPerimeter)
+	}
+
+	// diagonal
+	resultOfDiagonal := rectangle.Diagonal()
+	expectedOfDiagonal := 70.0
+	if resultOfDiagonal != expectedOfDiagonal {
+		t.Errorf("Got %f, Expected %f", expectedOfDiagonal, expectedOfDiagonal)
+	}
 
 }
