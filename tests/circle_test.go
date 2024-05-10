0c2f19e643f9864d80fe693b6d91ea48ffae8916
Francis Osei Annin <francisannin5@gmail.com>
2024-05-10 20:00:22 +0000

    Add: New updates

--- tests/circle_test.go
+++ tests/circle_test.go
@@ -6,14 +6,22 @@ import (
 )
 
 func TestCircle(t *testing.T) {
-	var circle = shape.Circle{60, 30}
+	var circle = shape.Circle{Diameter: 60, Radius: 30}
 
 	// area of a circle
 	resultOfAreaOfCircle := circle.Area()
-	expectedAreaOfCircle := 5
+	expectedAreaOfCircle := 5.0
 
-	if resultOfAreaOfCircle != expectedAreaOfCircle {
+	if expectedAreaOfCircle != resultOfAreaOfCircle {
+		t.Errorf("Got %v, expected %v", resultOfAreaOfCircle, expectedAreaOfCircle)
+	}
 
+	// circumference of a circle
+	resultCircumferenceOfCircle := circle.Circumference()
+	expectedCircumferenceOfCircle := 188.5714285714286
+
+	if resultCircumferenceOfCircle != expectedCircumferenceOfCircle {
+		t.Errorf("Got %v, expected %v", resultCircumferenceOfCircle, expectedCircumferenceOfCircle)
 	}
 
 }
