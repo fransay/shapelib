480fa13bca614f9b2cf3d591db83d70ce2220557
Francis Osei Annin <francisannin5@gmail.com>
2024-05-11 23:09:04 +0000

    Add: New updates

--- shape/octagon.go
+++ shape/octagon.go
@@ -15,3 +15,14 @@ func (o *Octagon) Area() float64 {
 func (o *Octagon) Perimeter() float64 {
 	return o.Side * 8
 }
+
+// Apothem returns the distance from the center of octagon to
+// the midpoint on any side.
+func (o *Octagon) Apothem() float64 {
+	return (o.Side / 2) * (math.Sqrt(2 + math.Sqrt(2)))
+}
+
+// InteriorDiagonal returns the length of the inner diameter
+func (o *Octagon) InteriorDiagonal() float64 {
+	return o.Side * math.Sqrt(4+2*math.Sqrt2)
+}
