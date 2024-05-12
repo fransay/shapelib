a32feeb6fc71189916ac35c2b8e94329cf8e6702
Francis Osei Annin <francisannin5@gmail.com>
2024-05-12 21:26:02 +0000

    Add: New updates

--- shape/hexagon.go
+++ shape/hexagon.go
@@ -1,10 +1,15 @@
 package shape
 
-import "math"
+import (
+	"math"
+	"shapelib/utils"
+)
 
 type Hexagon struct {
 	side float64
 }
+
+const numOfSides = 6
 
 // Area returns the area of regular hexagon
 func (h *Hexagon) Area() float64 {
@@ -13,7 +18,10 @@ func (h *Hexagon) Area() float64 {
 
 // Perimeter returns the perimeter of regular hexagon
 func (h *Hexagon) Perimeter() float64 {
-	return ((3 * math.Sqrt(3)) / 2) * math.Pow(h.side, 2)
+	return h.side * numOfSides
 }
 
-//
+// Apothem returns the apothem of a regular polygon
+func (h *Hexagon) Apothem() float64 {
+	return (h.side / 2) * utils.Cot(math.Pi/h.side)
+}
--- shape/nanogon.go
+++ shape/nanogon.go
@@ -14,3 +14,8 @@ func (n *Nanogon) Area() float64 {
 	const angle = math.Pi / 9
 	return 9 * math.Pow(n.side, 2) * utils.Cot(angle)
 }
+
+// Perimeter returns perimeter of nanogon
+func (n *Nanogon) Perimeter() float64 {
+	return n.side * 9
+}
