237a212a370c538af6b62dbe7da3cd23ebdeb508
Francis Osei Annin <francisannin5@gmail.com>
2024-05-22 21:16:51 +0000

    test: refactored test tree

--- coord-sys/test/cartesian_test.go
+++ coord-sys/test/cartesian_test.go
@@ -13,20 +13,22 @@ func TestToPolar(t *testing.T) {
 	obsRR, obsRD := cartObj.ToPolar()
 	expRR, expRD := cs.Polar{
 		Distance: 20.615528128088304, Angle: 0.24497866312686414},
-		cs.Polar{Distance: 20.615528128088304, Angle: 14.036243467926479}
+		cs.Polar{
+			Distance: 20.615528128088304, Angle: 14.036243467926479}
 	if obsRR != expRR && obsRD != expRD {
-		t.Errorf("expected %f, got %f", expRR, obsRR)
-		t.Errorf("expected %f, got %f", expRD, obsRD)
+		t.Errorf("Expected %f, Got %f", expRR, obsRR)
+		t.Errorf("Expected %f, Got %f", expRD, obsRD)
 	}
 
 	// obj 2
 	cartObj2 := cs.Cart2D{X: 0, Y: 1}
 	obsRR2, obsRD2 := cartObj2.ToPolar()
 	expRR2, expRD2 := cs.Polar{
-		Distance: 1.0, Angle: 1.5707963267948966}, cs.Polar{Distance: 1.0, Angle: 90}
+		Distance: 1.0, Angle: 1.5707963267948966},
+		cs.Polar{Distance: 1.0, Angle: 90}
 	if obsRR2 != expRR2 && obsRD2 != expRD2 {
-		t.Errorf("expected %f, got %f", expRR2, obsRR2)
-		t.Errorf("expected %f, got %f", expRD2, obsRD2)
+		t.Errorf("Expected %f, Got %f", expRR2, obsRR2)
+		t.Errorf("Expected %f, Got %f", expRD2, obsRD2)
 	}
 }
 
@@ -37,7 +39,7 @@ func TestPoint2PointDist(t *testing.T) {
 	obs := cartObj.Point2PointDistance2D(cs.Cart2D{X: 10, Y: 5})
 	exp := 10.0
 	if obs != exp {
-		t.Errorf("expected %f, got %f", exp, obs)
+		t.Errorf("Expected %f, Got %f", exp, obs)
 	}
 
 	// obj2
@@ -45,7 +47,7 @@ func TestPoint2PointDist(t *testing.T) {
 	obs2 := cartObj2.Point2PointDistance2D(cs.Cart2D{X: 50, Y: 10})
 	exp2 := math.Sqrt(925)
 	if obs != exp {
-		t.Errorf("expected %f, got %f", exp2, obs2)
+		t.Errorf("Expected %f, Got %f", exp2, obs2)
 	}
 }
 
@@ -55,7 +57,7 @@ func TestCartTranslate2D(t *testing.T) {
 	obs := cartObj.Translate2D([2]float64{1, 3})
 	exp := cs.Cart2D{X: 21.0, Y: 8.0}
 	if obs != exp {
-		t.Errorf("expected %f, got %f", exp, obs)
+		t.Errorf("Expected %f, Got %f", exp, obs)
 	}
 }
 
@@ -65,9 +67,8 @@ func TestCartTranslate3D(t *testing.T) {
 	obs := cartObj.Translate3D([3]float64{1, 3, 30})
 	exp := cs.Cart3D{X: 21.0, Y: 8.0, Z: 35.0}
 	if obs != exp {
-		t.Errorf("expected %f, got %f", exp, obs)
+		t.Errorf("Expected %f, Got %f", exp, obs)
 	}
-
 }
 
 // test rotate2D
@@ -76,9 +77,8 @@ func TestCartRotate2D(t *testing.T) {
 	obs := cartObj.Rotate2D(30.0)
 	exp := cs.Cart2D{X: 8.025187118215989, Y: -20.531889731295156}
 	if obs != exp {
-		t.Errorf("expected %f, got %f", exp, obs)
+		t.Errorf("Expected %f, Got %f", exp, obs)
 	}
-
 }
 
 // test rotate3D
@@ -90,5 +90,4 @@ func TestCartRotate3D(t *testing.T) {
 	if obs != exp {
 		t.Errorf("Expected %f, Got %f", exp, obs)
 	}
-
 }
