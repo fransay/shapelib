b63cd0f33a4ea3d208406b1b2176179cddd22059
Francis Osei Annin <francisannin5@gmail.com>
2024-08-19 21:31:30 +0000

    refactor

--- coord-sys/spherical/spherical.go
+++ coord-sys/spherical/spherical.go
@@ -12,25 +12,25 @@ type Spherical struct {
 	AzimuthAngle   float64 // in radians
 }
 
-// ToCartesianIn2D converts spherical coordinate to cartesian coordinates in 3D
-func (s *Spherical) ToCartesianIn2D() (cart cart2.Cartesian2D) {
+// ToCart2D converts spherical coordinate to cartesian coordinates in 3D
+func (s *Spherical) ToCart2D() (cart cart2.Cart2D) {
 	x := s.RadialDistance * math.Sin(s.AzimuthAngle) * math.Cos(s.PolarAngle)
 	y := s.RadialDistance * math.Sin(s.AzimuthAngle) * math.Sin(s.PolarAngle)
-	cart = cart2.Cartesian2D{X: x, Y: y}
+	cart = cart2.Cart2D{X: x, Y: y}
 	return cart
 }
 
-// ToCartesianIn3D converts spherical coordinate to cartesian coordinates in 3D
-func (s *Spherical) ToCartesianIn3D() (cart cart2.Cartesian3D) {
+// ToCart3D converts spherical coordinate to cartesian coordinates in 3D
+func (s *Spherical) ToCart3D() (cart cart2.Cart3D) {
 	x := s.RadialDistance * math.Sin(s.AzimuthAngle) * math.Cos(s.PolarAngle)
 	y := s.RadialDistance * math.Sin(s.AzimuthAngle) * math.Sin(s.PolarAngle)
 	z := s.RadialDistance * math.Cos(s.PolarAngle)
-	cart = cart2.Cartesian3D{}
+	cart = cart2.Cart3D{X: x, Y: y, Z: z}
 	return cart
 }
 
-// Point2PointDistance returns distance two spherical points using spherical laws of cosine
-func (s *Spherical) Point2PointDistance(point Spherical) (dist float64) {
+// Distance returns distance two spherical points using spherical laws of cosine
+func (s *Spherical) Distance(point Spherical) (dist float64) {
 	addRadialDist := s.RadialDistance + point.RadialDistance
 	prodRadialDist := s.RadialDistance * point.RadialDistance
 	angArithmetic := math.Sin(s.AzimuthAngle)*math.Sin(point.AzimuthAngle)*math.Cos(s.PolarAngle-point.PolarAngle) +
--- coord-sys/spherical/spherical_test.go
+++ coord-sys/spherical/spherical_test.go
@@ -8,7 +8,7 @@ import (
 // tests point to point distance spherical
 func TestSphericalPoint2PointDistance(t *testing.T) {
 	sphericalObject := Spherical{RadialDistance: 10.0, PolarAngle: 50.0, AzimuthAngle: 30.0}
-	observedDistance := sphericalObject.Point2PointDistance(Spherical{RadialDistance: 30.0, PolarAngle: 40.0, AzimuthAngle: 50.0})
+	observedDistance := sphericalObject.Distance(Spherical{RadialDistance: 30.0, PolarAngle: 40.0, AzimuthAngle: 50.0})
 	expectedDistance := 9.011182193785475
 	if observedDistance != expectedDistance {
 		t.Errorf("Expected %f, Got %f", expectedDistance, observedDistance)
@@ -16,10 +16,10 @@ func TestSphericalPoint2PointDistance(t *testing.T) {
 }
 
 // tests spherical to cartesian coordinate in two-dimension
-func TestCartesian2D(t *testing.T) {
+func TestCart2D(t *testing.T) {
 	sphericalObject := Spherical{RadialDistance: 10.0, PolarAngle: 50.0, AzimuthAngle: 30.0}
-	observedDistance := sphericalObject.ToCartesianIn2D()
-	expectedDistance := cartesian.Cartesian2D{X: cartesian.Axis{Start: 0, End: 10, Step: 2}, Y: cartesian.Axis{Start: 100, End: 500, Step: 50}}
+	observedDistance := sphericalObject.ToCart2D()
+	expectedDistance := cartesian.Cart2D{X: 675.65, Y: 43.54}
 	if observedDistance != expectedDistance {
 		t.Errorf("Expected %f, Got %f", expectedDistance, observedDistance)
 	}
@@ -28,9 +28,9 @@ func TestCartesian2D(t *testing.T) {
 // tests spherical to cartesian coordinate in three-dimension
 func TestSphericalToCartesian3D(t *testing.T) {
 	sphericalObject := Spherical{RadialDistance: 10.0, PolarAngle: 50.0, AzimuthAngle: 30.0}
-	observedDistance := sphericalObject.ToCartesianIn3D()
-	expectedDistance := cartesian.Cartesian2D{X: cartesian.Axis{}, Y: cartesian.Axis{}}
-	if observedDistance != expectedDistance {
+	observedDistance := sphericalObject.ToCart3D()
+	expectedDistance := cartesian.Cart3D{X: 343.5, Y: 433.43, Z: 344.3}
+	if expectedDistance != observedDistance {
 		t.Errorf("Expected %f, Got %f", expectedDistance, observedDistance)
 	}
 }
