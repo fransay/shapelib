2ad60ce1c92ec42b4fd62674722120655bf751f0
Francis Osei Annin <francisannin5@gmail.com>
2024-10-19 22:25:07 +0000

    update

--- props/surface_area.go
+++ props/surface_area.go
@@ -1,67 +1,66 @@
-package utils
+package props
 
 import (
 	"math"
 )
 
 // SurfAreaOfTetrahedron returns surface area of a tetrahedron
-func SurfAreaOfTetrahedron(sideLength float64) (surfArea float64) {
-	surfArea = math.Sqrt(3) * math.Pow(sideLength, 2)
-	return surfArea
+func SurfAreaOfTetrahedron(sideLength float64) float64 {
+	return math.Sqrt(3) * math.Pow(sideLength, 2)
 }
 
 // SurfAreaOfRightAngledPyramid returns surface area of a right-angled pyramid
-// pyramidHeight is also known as slant height.
-func SurfAreaOfRightAngledPyramid(baseArea float64, basePerimeter float64, pyramidHeight float64) (surfArea float64) {
-	surfArea = baseArea + basePerimeter*pyramidHeight/2
-	return surfArea
+func SurfAreaOfRightAngledPyramid(
+	baseArea float64,
+	basePerimeter float64,
+	pyramidHeight float64) float64 {
+	return baseArea + basePerimeter*pyramidHeight/2
 }
 
 // SurfAreaOfCube returns surface area of a cube
-func SurfAreaOfCube(sideLength float64) (surfArea float64) {
-	surfArea = 6 * sideLength
-	return surfArea
+func SurfAreaOfCube(sideLength float64) float64 {
+	return 6 * sideLength
 }
 
 // SurfAreaOfCuboid returns surface area of cuboid
-func SurfAreaOfCuboid(sideLength, sideWidth, sideHeight float64) (surfArea float64) {
-	surfArea = 2*(sideLength) + 2*(sideWidth) + 2*(sideHeight)
-	return surfArea
+func SurfAreaOfCuboid(
+	sideLength,
+	sideWidth,
+	sideHeight float64) float64 {
+	return 2*(sideLength) + 2*(sideWidth) + 2*(sideHeight)
 }
 
 // SurfAreaOfPentahedron returns the surface area of pentahedron
-func SurfAreaOfPentahedron(baseArea, basePerimeter, slantHeight float64) (sideArea float64) {
-	sideArea = baseArea + basePerimeter*slantHeight/2
-	return sideArea
-
+func SurfAreaOfPentahedron(
+	baseArea,
+	basePerimeter,
+	slantHeight float64) float64 {
+	return baseArea + basePerimeter*slantHeight/2
 }
 
 // SurfAreaOfHexahedron returns the surface area of hexahedron
-func SurfAreaOfHexahedron(sideLength float64) (surfArea float64) {
-	surfArea = 6 * (sideLength * sideLength)
-	return surfArea
+func SurfAreaOfHexahedron(sideLength float64) float64 {
+	return 6 * (sideLength * sideLength)
 }
 
 // SurfAreaOfHeptahedron returns the surface area of heptahedron
-func SurfAreaOfHeptahedron(apothem, slantHeight float64) (surfArea float64) {
-	surfArea = (7 * apothem * slantHeight) / 4
-	return surfArea
+func SurfAreaOfHeptahedron(
+	apothem,
+	slantHeight float64) float64 {
+	return (7 * apothem * slantHeight) / 4
 }
 
 // SurfAreaOfRightAngledCylinder returns the surface area of a right cylinder
-func SurfAreaOfRightAngledCylinder(radius, height float64) (surfArea float64) {
-	surfArea = 2*math.Pi*radius*height + 2*math.Pi*math.Pow(radius, 2)
-	return surfArea
+func SurfAreaOfRightAngledCylinder(radius, height float64) float64 {
+	return 2*math.Pi*radius*height + 2*math.Pi*math.Pow(radius, 2)
 }
 
 // SurfAreaOfSphere returns the surface area of a sphere
-func SurfAreaOfSphere(radius float64) (surfArea float64) {
-	surfArea = 4 * math.Pi * math.Pow(radius, 2)
-	return surfArea
+func SurfAreaOfSphere(radius float64) float64 {
+	return 4 * math.Pi * math.Pow(radius, 2)
 }
 
 // SurfAreaOfTorus returns the surface area of a Torus
-func SurfAreaOfTorus(innerRadius, outerRadius float64) (surfArea float64) {
-	surfArea = 4 * math.Pow(math.Pi, 2) * outerRadius * innerRadius
-	return surfArea
+func SurfAreaOfTorus(innerRadius, outerRadius float64) float64 {
+	return 4 * math.Pow(math.Pi, 2) * outerRadius * innerRadius
 }
--- props/volume.go
+++ props/volume.go
@@ -1,149 +1,128 @@
-package shape
+package props
 
 import (
 	"math"
 )
 
 // VolOfTetrahedron return volume of a tetrahedron
-func VolOfTetrahedron(side float64) (vol float64) {
-	vol = (side * side * side) / (6 * math.Sqrt(2))
-	return vol
+func VolOfTetrahedron(side float64) float64 {
+	return (side * side * side) / (6 * math.Sqrt(2))
 }
 
-// VolOfRightAngledTriPyramid volume of a right-angled pyramid
-func VolOfRightAngledTriPyramid(baseBaseTriangle, heightBaseTriangle, heightPyramid float64) (vol float64) {
-	vol = (baseBaseTriangle * heightBaseTriangle * heightPyramid) / 6
-	return vol
+// VolOfRightAngledTriPyramid return volume of a right-angled pyramid
+func VolOfRightAngledTriPyramid(
+	baseBaseTriangle,
+	heightBaseTriangle,
+	heightPyramid float64) float64 {
+	return (baseBaseTriangle * heightBaseTriangle * heightPyramid) / 6
 }
 
-// VolOfCube volume of a cube
-func VolOfCube(side float64) (vol float64) {
-	vol = side * side * side
-	return vol
+// VolOfCube return volume of a cube
+func VolOfCube(side float64) float64 {
+	return (side * side * side)
 }
 
-// VolOfCuboid volume of a cuboid
-func VolOfCuboid(length, width, height float64) (vol float64) {
-	vol = length * width * height
-	return vol
+// VolOfCuboid return volume of a cuboid
+func VolOfCuboid(length, width, height float64) float64 {
+	return length * width * height
 }
 
-// VolOfPentahedron volume of a pentahedron
-func VolOfPentahedron(lengthOfBase, height float64) (vol float64) {
-	vol = (math.Pow(lengthOfBase, 2) * height) / 3
-	return vol
+// VolOfPentahedron return volume of a pentahedron
+func VolOfPentahedron(lengthOfBase, height float64) float64 {
+	return (math.Pow(lengthOfBase, 2) * height) / 3
 }
 
-// VolOfSphere returns the volume of a sphere
-func VolOfSphere(radius float64) (vol float64) {
-	vol = (4 * math.Pi * math.Pow(radius, 3)) / 3
-	return vol
+// VolOfSphere return the volume of a sphere
+func VolOfSphere(radius float64) float64 {
+	return (4 * math.Pi * math.Pow(radius, 3)) / 3
 }
 
 // VolOfCone return volume of a cone
-func VolOfCone(radius, height float64) (vol float64) {
-	vol = (math.Pi * math.Pow(radius, 2) * height) / 3
-	return vol
+func VolOfCone(radius, height float64) float64 {
+	return (math.Pi * math.Pow(radius, 2) * height) / 3
 }
 
 // VolOfCylinder return volume of cylinder
-func VolOfCylinder(radius, height float64) (vol float64) {
-	vol = radius * height
-	return vol
+func VolOfCylinder(radius, height float64) float64 {
+	return radius * height
 }
 
 // VolOfRectPrism return volume of a rectangular prism
-func VolOfRectPrism(length, width, height float64) (vol float64) {
-	vol = length * width * height
-	return vol
+func VolOfRectPrism(length, width, height float64) float64 {
+	return length * width * height
 }
 
-// VolOfTriPrism return volume of triangular prism
-// with an equilateral base.
-func VolOfTriPrism(baseSideA, baseSideB, height float64) (vol float64) {
+// VolOfTriPrism return volume of triangular prism with an equilateral base.
+func VolOfTriPrism(baseSideA, baseSideB, height float64) float64 {
 	areaOfBase := baseSideA * baseSideB
-	vol = areaOfBase * height
-	return vol
+	return areaOfBase * height
 }
 
 // VolOfPentPrism return volume of pentagonal prism
-func VolOfPentPrism(baseEdge, height float64) (vol float64) {
+func VolOfPentPrism(baseEdge, height float64) float64 {
 	factor := math.Sqrt(5*(5+2*math.Sqrt(5))) / 4
-	vol = factor * math.Pow(baseEdge, 2) * height
-	return vol
+	return factor * math.Pow(baseEdge, 2) * height
 }
 
 // VolOfHexaPrism return volume of hexagonal prism
-func VolOfHexaPrism(baseEdge, height float64) (vol float64) {
-	vol = (3 * math.Sqrt(3)) / 2 * math.Pow(baseEdge, 2) * height
-	return vol
+func VolOfHexaPrism(baseEdge, height float64) float64 {
+	return (3 * math.Sqrt(3)) / 2 * math.Pow(baseEdge, 2) * height
 }
 
 // VolOfOctPrism return volume of octagonal prism
-func VolOfOctPrism(baseEdge, height float64) (vol float64) {
+func VolOfOctPrism(baseEdge, height float64) float64 {
 	factor := 2 * (1 + math.Sqrt(2))
-	vol = factor * math.Pow(baseEdge, 2) * height
-	return vol
+	return factor * math.Pow(baseEdge, 2) * height
 }
 
 // VolOfSquarePyramid return volume of square pyramid
-func VolOfSquarePyramid(baseEdge, height float64) (vol float64) {
-	vol = math.Pow(baseEdge, 2) * height / 3
-	return vol
+func VolOfSquarePyramid(baseEdge, height float64) float64 {
+	return math.Pow(baseEdge, 2) * height / 3
 }
 
 // VolOfTriangularPyramid return volume of a triangular pyramid
-func VolOfTriangularPyramid(areaOfBase, height float64) (vol float64) {
-	vol = (areaOfBase * height) / 3
-	return vol
+func VolOfTriangularPyramid(areaOfBase, height float64) float64 {
+	return (areaOfBase * height) / 3
 }
 
 // VolOfPentagonalPyramid return volume of pentagonal pyramid
-func VolOfPentagonalPyramid(baseEdge, height float64) (vol float64) {
-	vol = (5 * math.Tan(54.0) * height * math.Pow(baseEdge, 2)) / 12
-	return vol
+func VolOfPentagonalPyramid(baseEdge, height float64) float64 {
+	return (5 * math.Tan(54.0) * height * math.Pow(baseEdge, 2)) / 12
 }
 
 // VolOfHexagonalPyramid return volume of hexagonal pyramid
-func VolOfHexagonalPyramid(baseEdge, height float64) (vol float64) {
-	vol = (math.Sqrt(3) * math.Pow(baseEdge, 2) * height) / 2
-	return vol
+func VolOfHexagonalPyramid(baseEdge, height float64) float64 {
+	return (math.Sqrt(3) * math.Pow(baseEdge, 2) * height) / 2
 }
 
 // VolOfConeFrustum return volume of cone frustum
-func VolOfConeFrustum(radius, height float64) (vol float64) {
-	vol = math.Pi * math.Pow(radius, 2) * height / 3
-	return vol
+func VolOfConeFrustum(radius, height float64) float64 {
+	return math.Pi * math.Pow(radius, 2) * height / 3
 }
 
 // VolOfCyFrustum return volume of a cylinder frustum or right cylinder
-func VolOfCyFrustum(radius, height float64) (vol float64) {
-	vol = math.Pi * math.Pow(radius, 2) * height
-	return vol
+func VolOfCyFrustum(radius, height float64) float64 {
+	return math.Pi * math.Pow(radius, 2) * height
 }
 
 // VolOfEllipsoid return volume of ellipsoid
-func VolOfEllipsoid(semiAxisA, semiAxisB, semiAxisC float64) (vol float64) {
-	vol = (4 * math.Pi * semiAxisA * semiAxisB * semiAxisC) / 3
-	return vol
+func VolOfEllipsoid(semiAxisA, semiAxisB, semiAxisC float64) float64 {
+	return (4 * math.Pi * semiAxisA * semiAxisB * semiAxisC) / 3
 }
 
 // VolOfTorus return volume of a torus
-func VolOfTorus(majorRadius, minorRadius float64) (vol float64) {
-	vol = math.Pi * math.Pow(minorRadius, 2) * 2 * math.Pi * majorRadius
-	return vol
+func VolOfTorus(majorRadius, minorRadius float64) float64 {
+	return math.Pi * math.Pow(minorRadius, 2) * 2 * math.Pi * majorRadius
 }
 
 // VolOfDecahedron return volume of a decahedron
-func VolOfDecahedron(sides float64) (vol float64) {
-	factor := (15 + (7 * math.Sqrt(5.0))) / 4
-	vol = factor * math.Pow(sides, 3)
-	return vol
+func VolOfDecahedron(sides float64) (factor float64) {
+	factor = (15 + (7 * math.Sqrt(5.0))) / 4
+	return factor * math.Pow(sides, 3)
 }
 
 // VolOfIcosahedron return volume of an icosahedron
-func VolOfIcosahedron(side float64) (vol float64) {
-	factor := 5 * (3.0 + math.Sqrt(5.0)) / 12
-	vol = factor * math.Pow(side, 3)
-	return vol
+func VolOfIcosahedron(side float64) (factor float64) {
+	factor = 5 * (3.0 + math.Sqrt(5.0)) / 12
+	return factor * math.Pow(side, 3)
 }
