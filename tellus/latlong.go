6b721ee2cb12be418632a9b1628a2a9f79a8f256
Francis Osei Annin <francisannin5@gmail.com>
2024-10-20 21:44:57 +0000

    added new updates

--- tellus/latlong.go
+++ tellus/latlong.go
@@ -1,7 +1,36 @@
-package types
+package tellus
 
 // LatLong type
 type LatLong struct {
 	Latitude  float64 // in degrees
 	Longitude float64 // in degrees
 }
+
+// Initialise new LatLong object
+func NewLatLong(lat, long float64) LatLong {
+	return LatLong{Latitude: lat, Longitude: long}
+}
+
+// ToDMS return degree minute seconds represent f LatLong
+// which is in degree decimals (DD).
+func (l *LatLong) ToDMS() ([]float64, []float64) {
+	lat := l.Latitude
+	long := l.Longitude
+	latDMS := conv(lat)
+	longDMS := conv(long)
+	return latDMS, longDMS
+
+}
+
+// conv returns a conversion of f into whole, mins, secs
+func conv(f float64) (fr []float64) {
+	fInt := int(f)
+	wholeDec := f - float64(fInt)
+	mInt := int(wholeDec * 60.0)
+	minuteDec := (wholeDec * 60.0) - float64(mInt)
+	sInt := minuteDec * 60.0
+	fr = []float64{float64(fInt), float64(mInt), float64(sInt)}
+	return fr
+}
+
+// todo: distance, bearing, geodesic, time zones converter.
