4636e3a45075d9cc004d0b967f6ba076573b5e75
Francis Osei Annin <francisannin5@gmail.com>
2025-05-11 11:32:57 +0000

    add area and test update; failed test.

--- src/props/area.go
+++ src/props/area.go
@@ -6,29 +6,27 @@ import (
 	"github.com/fransay/shapelib/src/geom"
 )
 
-// AreaOfTriangleBH returns area of a triangle given the base and the height
-func AreaOfTriangleBH(base, height float64) (area float64) {
-	area = 0.5 * base * height
+// AreaOfTriangleGivenBH returns area of a triangle given the base and the height
+func AreaOfTriangleGivenBH(base, height float64) (area float64) {
+	area = (base * height) / 2
 	return area
 }
 
-// AreaOfTriangleSide returns the area of a triangle given all three sides
-func AreaOfTriangleSide(side1, side2, side3 float64) (area float64) {
-	averageSides := (side1 + side2 + side3) / 3
-	compute := averageSides * (averageSides - side1) * (averageSides - side2) * (averageSides - side3)
-	area = math.Sqrt(compute)
+// AreaOfTriangleGivenSide returns the area of a triangle given all three sides
+func AreaOfTriangleGivenSide(side1, side2, side3 float64) (area float64) {
+	semiPerimeter := (side1 + side2 + side3) / 2
+	area = math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3))
 	return area
-
 }
 
-// AreaOfTriangleAngle returns the area of a triangle given two sides and an angle
-func AreaOfTriangleAngle(side1, side2, angle float64) (area float64) {
-	area = (side1 * side2) * math.Sin(angle) / 2
+// AreaOfTriangleGivenAngle returns the area of a triangle given two sides and an angle
+func AreaOfTriangleGivenAngle(side1, side2, angle float64) (area float64) {
+	area = ((side1 * side2) * math.Sin(angle)) / 2
 	return area
 
 }
 
-// AreaOfSquare :return the area of square given the length
+// AreaOfSquare return the area of square given the length
 func AreaOfSquare(length float64) (area float64) {
 	area = length * length
 	return area
@@ -54,7 +52,7 @@ func AreaOfKite(diag1, diag2 float64) (area float64) {
 
 // AreaOfRhombus returns the area of a rhombus given the main diagonals
 func AreaOfRhombus(diag1, diag2 float64) (area float64) {
-	area = AreaOfKite(diag1, diag2)
+	area = (diag1 + diag2) / 2
 	return area
 }
 
@@ -64,43 +62,43 @@ func AreaOfTrapezoid(side1, side2, height float64) (area float64) {
 	return area
 }
 
-// AreaOfRPentagon returns the area of a regular pentagon given the side
-func AreaOfRPentagon(side float64) (area float64) {
+// AreaOfPentagon returns the area of a regular pentagon given the side
+func AreaOfPentagon(side float64) (area float64) {
 	area = 0.25 * math.Sqrt(5.0*(5.0+(2.0*math.Sqrt(5.0)))) * (side * side)
 	return area
 
 }
 
-// AreaOfRHexagon returns the area of a regular hexagon given the side
-func AreaOfRHexagon(side float64) (area float64) {
+// AreaOfHexagon returns the area of a regular hexagon given the side
+func AreaOfHexagon(side float64) (area float64) {
 	area = 1.5 * math.Sqrt(3) * math.Pow(side, 2)
 	return area
 
 }
 
-// AreaOfRHeptagon returns the area of a regular heptagon given the side
-func AreaOfRHeptagon(side float64) (area float64) {
+// AreaOfHeptagon returns the area of a regular heptagon given the side
+func AreaOfHeptagon(side float64) (area float64) {
 	const apothem float64 = 3.634
 	area = apothem * side * side
 	return area
 }
 
-// AreaOfROctagon returns the area of a regular octagon given the side
-func AreaOfROctagon(side float64) (area float64) {
+// AreaOfOctagon returns the area of a regular octagon given the side
+func AreaOfOctagon(side float64) (area float64) {
 	area = 2 * (1 + math.Sqrt(2.0)) * (side * side)
 	return area
 }
 
-// AreaOfRNonagon returns the area of a regular nonagon given the side
-func AreaOfRNonagon(side float64) (area float64) {
+// AreaOfNonagon returns the area of a regular nonagon given the side
+func AreaOfNonagon(side float64) (area float64) {
 	const cotangent = 2.747477
 	area = (9 / 4) * math.Pow(side, 2.0) * cotangent
 	return area
 
 }
 
-// AreaOfRDecagon returns the area of a regular decagon given the side
-func AreaOfRDecagon(side float64) (area float64) {
+// AreaOfDecagon returns the area of a regular decagon given the side
+func AreaOfDecagon(side float64) (area float64) {
 	area = 2.5 * math.Pow(side, 2.0) * math.Sqrt(5.0+2*math.Sqrt(5.0))
 	return area
 }
@@ -141,12 +139,12 @@ func AreaOfEllipse(semiMajorAxis, semiMinorAxis float64) (area float64) {
 	return area
 }
 
-// AreaCoordinates returns the area of any two-dimensional shape given the coordinates of the vertices, using the shoelace algorithm...
-func AreaCoordinates(cords ...geom.Point2D) float64 {
+// AreaGivenCoordinates returns the area of any two-dimensional shape given the coordinates of the vertices, using the shoelace algorithm...
+func AreaGivenCoordinates(cords ...geom.Point2D) float64 {
 	var forPass float64
 	var backPass float64
 	var numberOfCords = len(cords) - 1
-	for i, j := 0, 1; j <= numberOfCords; i, j = i+1, j+1 {
+	for i, j := 0, 1; j <= numberOfCords; i, j = i+1, j+1 { // todo: fix area given coordinates
 		forPass += cords[i].Y * cords[j].X
 		backPass += cords[j].Y * cords[i].X
 	}
--- src/props/area_test.go
+++ src/props/area_test.go
@@ -1,152 +1,111 @@
 package props
 
 import (
+	"fmt"
+	"github.com/franela/goblin"
+	"github.com/fransay/shapelib/internal/utils"
 	"math"
 	"testing"
 
 	"github.com/fransay/shapelib/src/geom"
 )
 
-// Test area of triangle given base and height
-func TestAreaOfTriangleBH(t *testing.T) {
-	res1 := AreaOfTriangleBH(10.0, 5.0)
-	expected1 := 25.0
-	if res1 != expected1 {
-		t.Errorf("Expected %f, Got %f", expected1, res1)
-	}
-	res2 := AreaOfTriangleBH(15.0, 2.0)
-	expected2 := 15.0
-	if res2 != expected2 {
-		t.Errorf("Expected %f, Got %f", expected2, res2)
-	}
-	res3 := AreaOfTriangleBH(1.0, 1.0)
-	expected3 := 0.5
-	if res3 != expected3 {
-		t.Errorf("Expected %f, Got %f", expected3, res3)
-	}
-	res4 := AreaOfTriangleBH(1.0, 5.0)
-	expected4 := 2.5
-	if res4 != expected4 {
-		t.Errorf("Expected %f, Got %f", expected4, res4)
-	}
+func TestArea(t *testing.T) {
+	g := goblin.Goblin(t)
+	g.Describe("Area", func() {
+		g.It("Area of TriangleBH", func() {
+			areaOfTriangleBH := AreaOfTriangleGivenBH(15, 20)
+			g.Assert(areaOfTriangleBH).Equal(150.0)
+		})
 
-}
+		g.It("Area of Triangle given their sides", func() {
+			areaOfTriangleGivenSide := AreaOfTriangleGivenSide(12.0, 8.0, 10.0)
+			isClose := utils.IsClose(areaOfTriangleGivenSide, 56.57, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
 
-// Test area of a triangle given the sides
-func TestAreaOfTriangleSide(t *testing.T) {
-	res := AreaOfTriangleSide(12.0, 8.0, 10.00)
-	exp := 0.0 // inaccuracies increase
-	if exp != res {
-		t.Errorf("Expected %f, Got %f", exp, res)
-	}
+		g.It("Area of Triangle given their angle", func() {
+			areaOfTriangleGivenAngle := AreaOfTriangleGivenAngle(10.0, 5.0, 120.0)
+			isClose := utils.IsClose(areaOfTriangleGivenAngle, 14.515280, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
 
-}
+		g.It("Area of Square", func() {
+			areaOfSquare := AreaOfSquare(5.0)
+			g.Assert(areaOfSquare).Equal(25.0)
+		})
 
-// Test area of a triangle given the angles
-func TestAreaOfTriangleAngle(t *testing.T) {
-	res := AreaOfTriangleAngle(10.00, 5.00, 120.00)
-	exp := math.Trunc(14.515280)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f, Got %f", exp, res)
-	}
+		g.It("Area of a Rectangle", func() {
+			areaOfRectangle := AreaOfRectangle(10.0, 5.0)
+			g.Assert(areaOfRectangle).Equal(50.0)
+		})
 
-}
+		g.It("Area of Parallelogram", func() {
+			areaOfParallelogram := AreaOfParallelogram(10.0, 5.0)
+			g.Assert(areaOfParallelogram).Equal(50.0)
+		})
 
-// Test area of a square
-func TestAreaOfSquare(t *testing.T) {
-	res := AreaOfSquare(5.0)
-	exp := math.Trunc(25.0)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f Got %f", exp, res)
-	}
-}
+		g.It("Area of Kite", func() {
+			areaOfKite := AreaOfKite(10.0, 5.0)
+			g.Assert(areaOfKite).Equal(25.0)
+		})
 
-// Test area of a rectangle
-func TestAreaOfRectangle(t *testing.T) {
-	res := AreaOfRectangle(10.00, 5.00)
-	exp := math.Trunc(50.00)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f, Got %f", exp, res)
-	}
-}
+		g.It("Area of Rhombus", func() {
+			areaOfRhombus := AreaOfRhombus(10.0, 5.0)
+			g.Assert(areaOfRhombus).Equal(25.0)
+		})
 
-// Test area of a parallelogram
-func TestAreaOfParallelogram(t *testing.T) {
-	res := AreaOfParallelogram(20.0, 10.0)
-	exp := math.Trunc(200.00)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f, Got %f", exp, res)
-	}
-}
+		g.It("Area of Trapezoid", func() {
+			areaOfTrapezoid := AreaOfTrapezoid(10.0, 5.0, 5.0)
+			g.Assert(areaOfTrapezoid).Equal(37.5)
+		})
 
-// Test area of a rhombus
-func TestAreaOfRhombus(t *testing.T) {
-	res := AreaOfRhombus(10, 5)
-	exp := math.Trunc(7.5)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f, Got %f", exp, res)
-	}
-}
+		g.It("Area of Pentagon", func() {
+			areaOfPentagon := AreaOfPentagon(5.0)
+			isClose := utils.IsClose(areaOfPentagon, 43.01194, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
 
-// Test area of a trapezoid
-func TestAreaOfTrapezoid(t *testing.T) {
-	res := AreaOfTrapezoid(10.0, 5.0, 5.0)
-	exp := math.Trunc(37.0)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f, Got %f", exp, res)
-	}
+		g.It("Area of Hexagon", func() {
+			areaOfHexagon := AreaOfHexagon(5.0)
+			isClose := utils.IsClose(areaOfHexagon, 259.80762, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
+		g.It("Area of Heptagon", func() {
+			areaOfHeptagon := AreaOfHeptagon(5.0)
+			fmt.Println("area of heptagon", areaOfHeptagon)
+			isClose := utils.IsClose(areaOfHeptagon, 90.84781, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
+		g.It("Area of Octagon", func() {
+			areaOfOctagon := AreaOfOctagon(5.0)
+			isClose := utils.IsClose(areaOfOctagon, 120.71, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
+		g.It("Area of Nonagon", func() {
+			areaOfNonagon := AreaOfNonagon(5.0)
+			isClose := utils.IsClose(areaOfNonagon, 154.5456, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
+		g.It("Area of Decagon", func() {
+			areaOfDecagon := AreaOfDecagon(5.0)
+			isClose := utils.IsClose(areaOfDecagon, 192.35522, 0.001)
+			g.Assert(isClose).IsTrue()
+		})
+		g.It("Area of Dodecagon", func() {})
+		g.It("Area of Circle", func() {})
+		g.It("Area of Semi-Circle", func() {})
+		g.It("Area of Quad-Circle", func() {})
+		g.It("Area of Oval", func() {})
+		g.It("Area of Ellipse", func() {})
+		g.It("Area By Coordinates", func() {})
 
-}
-
-// Test area of a regular pentagon
-func TestAreaOfRPentagon(t *testing.T) {
-	res := AreaOfRPentagon(5.0)
-	exp := math.Trunc(43.01194)
-	if math.Trunc(res) != exp {
-		t.Errorf(" Expected %f, Got %f", exp, res)
-	}
-}
-
-// Test area of a regular hexagon
-func TestAreaOfRHexagon(t *testing.T) {
-	res := AreaOfRHexagon(10.0)
-	exp := math.Trunc(259.81)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f, Got %f", exp, res)
-	}
-}
-
-// Test area of a regular octagon
-func TestAreaOfROctagon(t *testing.T) {
-	res := AreaOfROctagon(5.00)
-	exp := math.Trunc(120.7)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f Got %f", exp, res)
-	}
-
-}
-
-// Test area of a regular nonagon
-func TestAreaOfRNonagon(t *testing.T) {
-	res := AreaOfRNonagon(5)
-	exp := math.Trunc(137.373850)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f Got %f", exp, res)
-	}
-}
-
-// Test area of a regular decagon
-func TestAreaOfRDecagon(t *testing.T) {
-	res := AreaOfRDecagon(5.00)
-	exp := math.Trunc(192.355)
-	if math.Trunc(res) != exp {
-		t.Errorf("Expected %f Got %f", exp, res)
-	}
+	})
 }
 
 // Test area of a regular heptagon
 func TestAreaOfRHeptagon(t *testing.T) {
-	res := AreaOfRHeptagon(5.00)
+	res := AreaOfHeptagon(5.00)
 	exp := math.Trunc(90.85)
 	if math.Trunc(res) != exp {
 		t.Errorf("Expected %f Got %f", exp, res)
@@ -211,7 +170,7 @@ func TestAreaOfEllipse(t *testing.T) {
 
 // Test area given coordinates
 func TestAreaCoordinates(t *testing.T) {
-	var res = AreaCoordinates(geom.Point2D{X: 1, Y: 4}, geom.Point2D{X: 3, Y: 6}, geom.Point2D{X: 8, Y: 5})
+	var res = AreaGivenCoordinates(geom.Point2D{X: 1, Y: 4}, geom.Point2D{X: 3, Y: 6}, geom.Point2D{X: 8, Y: 5})
 	var exp = math.Trunc(-19.500)
 	if math.Trunc(res) != exp {
 		t.Errorf("Expected %f Got %f", exp, res)
