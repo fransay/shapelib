21f94145e4513f907b7a1d4f3bc567a0026b3149
Francis Osei Annin <francisannin5@gmail.com>
2025-10-20 21:44:08 +0000

    completed triangle implementation

--- src/shape/triangle.go
+++ src/shape/triangle.go
@@ -12,14 +12,14 @@ const (
 	Scalene     = "Scalene"
 )
 
-// Triangle defines a triangle given all sides/length
+// Triangle defines a triangle given all sides/length.
 type Triangle struct {
-	SideOne   float64 // first side
-	SideTwo   float64 // second side
-	SideThree float64 // three side
+	SideOne   float64
+	SideTwo   float64
+	SideThree float64
 }
 
-// NewTriangle initialise a new Triangle object
+// NewTriangle initialise a new Triangle object given its sides.
 func NewTriangle(sideOne, sideTwo, sideThree float64) *Triangle {
 	return &Triangle{
 		SideOne:   sideOne,
@@ -31,9 +31,7 @@ func NewTriangle(sideOne, sideTwo, sideThree float64) *Triangle {
 // Area returns the computed area of triangle given sides
 func (t *Triangle) Area() (areaBySides float64) {
 	semiPerimeter := (t.SideOne + t.SideTwo + t.SideThree) / 2
-	areaBySides = math.Sqrt(
-		semiPerimeter * (semiPerimeter - t.SideOne) *
-			(semiPerimeter - t.SideTwo) * (semiPerimeter - t.SideThree))
+	areaBySides = math.Sqrt(semiPerimeter * (semiPerimeter - t.SideOne) * (semiPerimeter - t.SideTwo) * (semiPerimeter - t.SideThree))
 	return areaBySides
 }
 
@@ -53,14 +51,20 @@ func (t *Triangle) SemiPerimeter() (perimeter float64) {
 func (t *Triangle) Type() (triangleType string) {
 	if utils.CompareThreeFloat64(t.SideOne, t.SideTwo, t.SideThree) {
 		triangleType = Equilateral
-	} else if utils.CompareTwoFloat64(t.SideOne, t.SideTwo) ||
-		utils.CompareTwoFloat64(t.SideTwo, t.SideThree) ||
-		utils.CompareTwoFloat64(t.SideOne, t.SideThree) {
+	} else if utils.CompareTwoFloat64(t.SideOne, t.SideTwo) || utils.CompareTwoFloat64(t.SideTwo, t.SideThree) || utils.CompareTwoFloat64(t.SideOne, t.SideThree) {
 		triangleType = Isosceles
-	} else if !utils.CompareTwoFloat64(t.SideOne, t.SideTwo) ||
-		!utils.CompareTwoFloat64(t.SideTwo, t.SideThree) ||
-		!utils.CompareTwoFloat64(t.SideOne, t.SideThree) {
+	} else if !utils.CompareTwoFloat64(t.SideOne, t.SideTwo) || !utils.CompareTwoFloat64(t.SideTwo, t.SideThree) || !utils.CompareTwoFloat64(t.SideOne, t.SideThree) {
 		triangleType = Scalene
 	}
 	return triangleType
+}
+
+// InRadius returns the radius of an inner circle
+func (t *Triangle) InRadius() float64 {
+	return t.Area() / t.SemiPerimeter()
+}
+
+// CircumRadius returns the radius of an exterior circle
+func (t *Triangle) CircumRadius() float64 {
+	return (t.SideOne * t.SideTwo * t.SideThree) / (4 * t.Area())
 }
--- src/shape/triangle_test.go
+++ src/shape/triangle_test.go
@@ -14,25 +14,41 @@ func TestTriangle(t *testing.T) {
 		60.0,
 		30.0)
 	g.Describe("Test Triangle", func() {
-		g.It("Area by sides", func() {
+		g.It("Test Triangle By Sides", func() {
 			areaBySides := triangle.Area()
 			isClose := utils.IsClose(areaBySides, 748.3314773547883, 0.0001)
 			g.Assert(isClose).Equal(true)
 		})
 
-		g.It("Area by perimeter", func() {
+		g.It("Test Triangle Perimeter", func() {
 			perimeter := triangle.Perimeter()
 			isClose := utils.IsClose(perimeter, 140.0, 0.0001)
 			g.Assert(isClose).Equal(true)
 		})
 
-		g.It("Area by Type", func() {
+		g.It("Test Triangle Type", func() {
 			scaleneTriangle := triangle.Type()
 			g.Assert(scaleneTriangle).Equal("Scalene")
 			equilateralTriangle := NewTriangle(10.0, 10.0, 10.0).Type()
 			g.Assert(equilateralTriangle).Equal("Equilateral")
 			isoscelesTriangle := NewTriangle(10.0, 10.0, 5.0).Type()
 			g.Assert(isoscelesTriangle).Equal("Isosceles")
+		})
+		g.It("Test SemiPerimeter", func() {
+			triangleSemiPerimeter := triangle.SemiPerimeter()
+			isClose := utils.IsClose(triangleSemiPerimeter, 70.0, 0.0001)
+			g.Assert(isClose).Equal(true)
+		})
+
+		g.It("Test InRadius", func() {
+			triangleInRadius := triangle.InRadius()
+			isClose := utils.IsClose(triangleInRadius, 10.690449676496975, 0.0001)
+			g.Assert(isClose).Equal(true)
+		})
+		g.It("Test CircumRadius", func() {
+			triangleCircumRadius := triangle.CircumRadius()
+			isClose := utils.IsClose(triangleCircumRadius, 30.066889715147745, 0.0001)
+			g.Assert(isClose).Equal(true)
 		})
 	})
 
